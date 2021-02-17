404e582ba31503dfd0e178972215f4f649bba037
soaelee <soae0923@gmail.com>
2021-02-17 18:49:21 +0900

    Test: React-hook-form with useForm

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,16 +1,46 @@
-import React from 'react'
-import { isLoggedInVar } from '../apollo'
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { isLoggedInVar } from '../apollo';
+
+interface IForm {
+  email: string;
+  password: string;
+}
 
 export const LoggedOutRouter = () => {
-  const onClick = () => {
-    isLoggedInVar(true);
-  }
+  const { register, watch, handleSubmit, errors } = useForm<IForm>();
+  const onSubmit = () => {
+    console.log(watch());
+  };
+  const onInvalid = () => {
+    console.log('can not create account');
+  };
+
   return (
     <div>
       <h1>Logged Out</h1>
-      <button onClick={onClick}>Click to login</button>
+      <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
+        <div>
+          <input
+            ref={register({
+              required: 'This is required',
+              pattern: /^[A-Za-z0-9._%+-]+@gmail.com$/,
+            })}
+            name="email"
+            placeholder="email"
+          />
+          {errors.email?.message && <span className="font-light text-sm text-red-600">{errors.email?.message}</span>}
+          {errors.email?.type === 'pattern' && (
+            <span className="font-light text-sm text-red-600">Only gmail allowed</span>
+          )}
+        </div>
+        <div>
+          <input ref={register({ required: true })} name="password" placeholder="password" />
+        </div>
+        <button className="bg-yellow-300">Submit</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
 // local-only-field를 업데이트하는 주체! not app but here
