66e6faf60f81711ebff48d685d721cf83cd37418
soaelee <soae0923@gmail.com>
2021-02-27 19:22:23 +0900

    Add: Order page gql, ui

--- src/pages/user/order.tsx
+++ src/pages/user/order.tsx
@@ -1,5 +1,6 @@
 import { gql, useQuery } from '@apollo/client';
 import React from 'react';
+import { Helmet } from 'react-helmet-async';
 import { useParams } from 'react-router-dom';
 import { getOrderQu, getOrderQuVariables } from '../../__api__/getOrderQu';
 
@@ -39,11 +40,27 @@ export const Order = () => {
       },
     },
   });
-  console.log(data);
-  console.log(id);
   return (
-    <div>
-      <span>Order</span>
+    <div className="container mt-32 flex justify-center ">
+      <Helmet>
+        <title>Order #{id} | Suber Eats</title>
+      </Helmet>
+      <div className="w-full max-w-screen-sm justify-center border border-gray-800 flex flex-col">
+        <h4 className="bg-gray-800 w-full py-5 text-white text-center text-xl">Order #{id}</h4>
+        <h5 className="p-5 py-10 text-3xl text-center">${data?.getOrder.order?.total}</h5>
+        <div className="p-6 text-xl grid gap-6">
+          <div className="border-t pt-5 border-gray-700">
+            Prepared By: <span className="font-medium">{data?.getOrder.order?.restaurant?.name}</span>
+          </div>
+          <div className="border-t pt-5 border-gray-700 ">
+            Deliver To: <span className="font-medium">{data?.getOrder.order?.customer?.email}</span>
+          </div>
+          <div className="border-t border-b py-5 border-gray-700">
+            Driver: <span className="font-medium">{data?.getOrder.order?.driver?.email || 'Not yet.'}</span>
+          </div>
+        </div>
+        <span className=" text-center mt-5 mb-3  text-2xl text-lime-600">Status: {data?.getOrder.order?.status}</span>
+      </div>
     </div>
   );
 };
