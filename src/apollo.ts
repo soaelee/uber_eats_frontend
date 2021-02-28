cd45a71c6df5b151ae5ecf898e95dfd9e2143be3
soaelee <soae0923@gmail.com>
2021-02-28 16:34:26 +0900

    Config: Subscription

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,11 +1,23 @@
-import { ApolloClient, createHttpLink, InMemoryCache, makeVar } from '@apollo/client';
+import { ApolloClient, createHttpLink, InMemoryCache, makeVar, split } from '@apollo/client';
 import { LOCALSTORAGE_TOKEN } from './constants';
 import { setContext } from '@apollo/client/link/context';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
 
 const token = localStorage.getItem(LOCALSTORAGE_TOKEN);
 
 export const isLoggedInVar = makeVar(Boolean(token)); //false
 export const authTokenVar = makeVar(token); //null
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      'x-jwt': authTokenVar() || '',
+    },
+  },
+});
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000/graphql',
@@ -20,8 +32,17 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
+  },
+  wsLink, //true
+  authLink.concat(httpLink) //false
+);
+
 export const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link: splitLink,
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
